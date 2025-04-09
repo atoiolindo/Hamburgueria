8342955a8f6dd16c0047658303c62cb8ac3963d1
atoiolindo <antoniomiguelandrade11@gmail.com>
2025-04-09 09:45:02 -0300

    listar clientes

---
+++ controle/funcoes.php
@@ -0,0 +1,17 @@
+<?php
+
+function listarCliente($conexao) {
+    $sql = "SELECT * FROM cliente";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_execute($comando);
+    $resultado = mysqli_stmt_get_result($comando);
+
+    $lista_clientes = [];
+    while ($cliente = mysqli_fetch_assoc($resultado)) {
+        $lista_clientes[] = $cliente;
+    }
+
+    mysqli_stmt_close($comando);
+    return $lista_clientes;
+};
