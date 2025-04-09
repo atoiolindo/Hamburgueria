47f33957e1046faef22e1ab2374f5a56023a0eef
atoiolindo <antoniomiguelandrade11@gmail.com>
2025-04-09 09:50:46 -0300

    salvar/cadastrar cliente

--- controle/funcoes.php
+++ controle/funcoes.php
@@ -15,3 +15,18 @@ function listarCliente($conexao) {
     mysqli_stmt_close($comando);
     return $lista_clientes;
 };
+
+function salvarCliente($conexao, $nome, $telefone, $endereco) {
+    $sql = "INSERT INTO cliente (nome, telefone, endereco) VALUES (?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'sss', $nome, $telefone, $endereco);
+    
+    mysqli_stmt_execute($comando);
+    
+    $idcliente = mysqli_stmt_insert_id($comando);
+
+    mysqli_stmt_close($comando);
+
+    return $idcliente;
+};
