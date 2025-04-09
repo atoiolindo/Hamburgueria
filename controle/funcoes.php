e259847aca9606cec28fce2fc2b54bcf54663025
isamin <liroyasmin@gmail.com>
2025-04-09 10:19:48 -0300

    salvar usuario

--- controle/funcoes.php
+++ controle/funcoes.php
@@ -170,6 +170,17 @@ function deletarFuncionario($conexao, $idfuncionario) {
     return $funcionou; 
 };
 
+function salvarUsuario($conexao, $nome, $email, $senha) {
+    $sql = "INSERT INTO tb_usuario (nome, email, senha) VALUES (?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+    $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
+    mysqli_stmt_bind_param($comando, 'sss', $nome, $email, $senha_hash);
+
+    $funcionou = mysqli_stmt_execute($comando);
+
+   mysqli_stmt_close($comando);
+    return $funcionou;
+};
 
 ?>
