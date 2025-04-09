3e77d830ae231a0bdaad84e28df42e3989a438c8
isamin <liroyasmin@gmail.com>
2025-04-09 09:52:23 -0300

    funcionarios

--- controle/funcoes.php
+++ controle/funcoes.php
@@ -15,3 +15,19 @@ function listarCliente($conexao) {
     mysqli_stmt_close($comando);
     return $lista_clientes;
 };
+
+function listarFuncionario($conexao) {
+    $sql = "SELECT * FROM funcionario";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_execute($comando);
+    $resultado = mysqli_stmt_get_result($comando);
+
+    $lista_funcionarios = [];
+    while ($funcionario = mysqli_fetch_assoc($resultado)) {
+        $lista_funcionarios[] = $funcionario;
+    }
+
+    mysqli_stmt_close($comando);
+    return $lista_funcionarios;
+};
