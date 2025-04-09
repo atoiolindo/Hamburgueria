126ec0a5803ab1c88ac0c8be8701a86120bf3e7b
gab99999 <163896436+gab99999@users.noreply.github.com>
2025-04-09 10:29:32 -0300

    mudei o banco
    tirei nascimento e coloquei nome no usuario

--- banco.sql
+++ banco.sql
@@ -135,6 +135,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `hamburgueria`.`usuario` (
   `idusuario` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `senha` VARCHAR(45) NOT NULL,
   UNIQUE INDEX `email_UNIQUE` (`idusuario` ASC) VISIBLE,
