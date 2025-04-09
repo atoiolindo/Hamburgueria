0ad06e9a1cedc51e1b9d73dcb0838b6dcd33cd8a
isamin <liroyasmin@gmail.com>
2025-04-09 10:28:35 -0300

    deslogar

---
+++ controle/deslogar.php
@@ -0,0 +1,5 @@
+<?php
+session_start(); // Inicia a sessão
+session_destroy(); // Destrói todas as variáveis de sessão
+header("Location: ../public/index.php"); // Redireciona o usuário para a página inicial
+?>
