c0366f012e1c9be02f98dff1e8b05788d6329e02
isasmin <liroyasmin@gmail.com>
2025-04-30 08:19:02 -0300

    edsfazer teste

--- controle/conexao.php
+++ controle/conexao.php
@@ -7,14 +7,3 @@ $banco = "hamburgueria";
 $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    teste
-</body>
-</html>
