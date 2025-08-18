84a0dc2fc658903516fc97a78782eedaebb85c4e
isasmin <liroyasmin@gmail.com>
2025-08-18 16:19:44 -0300

    criando formVenda.php

---
+++ codigo/public/formVenda.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="salvarVenda.php">
+        Cliente: <br>
+        <select name="idcliente" id="idcliente">
+            <?php
+            require_once "../controle/conexao.php";
+            require_once "../controle/funcoes.php";
+
+            $lista_clientes = listarCliente($conexao);
+
+            foreach ($lista_clientes as $cliente) {
+                $idcliente = $cliente['idcliente'];
+                $nome = $cliente['nome'];
+
+                echo "<option value='$idcliente'>$nome</option>";
+            }
+            ?>
+        </select>
+
+        <br><br>
+
+        Data: <br>
+        <input type="date" name="data_compra"><br><br>
+
+        Produtos: <br>
+        <?php
+        $lista_produtos = listarProduto ($conexao);
+
+        foreach ($lista_produtos as $produto) {
+            $idproduto = $produto['idproduto'];
+            $nome = $produto['nome'];
+            $preco = $produto['preco_venda'];
+
+            echo "<input type='checkbox' value='$idproduto' id='marcado[$idproduto]' name='idproduto[]'>R$ <span id='preco[$idproduto]'>$preco</span> - $nome ";
+            echo "<input type='text' name='quantidade[$idproduto]' id='quantidade[$idproduto]' value='0' onchange='calcular()'><br>";
+        }
+        ?>
+        <br>
+        Valor Total: <br>
+        <input type="text" name="valor_total" id="valor_total" disabled><br><br>
+
+        <input type="submit" value="Registrar Venda"> <br>
+    
+
+    </form>
+    <button>Botão de testes</button>
+</body>
+
+</html>
