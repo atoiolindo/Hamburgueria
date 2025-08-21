e8a4a41f13766a525eab52c3704f2427c96c5d3b
isasmin <liroyasmin@gmail.com>
2025-08-21 13:36:54 -0300

    arruei verifica logado

--- codigo/controle/verificaLogado.php
+++ codigo/controle/verificaLogado.php
@@ -1,9 +1,8 @@
-<?php 
-session_start()
+<?php
+session_start();
 
-    if (!isset($_SESSION['logado'])) {
-        echo "não posso acessar";
-        header("Location: ../public/index.php");
-    }
-
+if (!isset($_SESSION['logado'])) {
+    // echo "não posso acessar";
+    header("Location: ../public/index.php");
+}
 ?>
