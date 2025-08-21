37c2336b8203a165a8d7f39806f8b5c5a8ecdcd6
atoiolindo <antoniomiguelandrade11@gmail.com>
2025-08-21 14:00:30 -0300

    pagina home

--- codigo/public/home.php
+++ codigo/public/home.php
@@ -6,6 +6,38 @@
     <title>Document</title>
 </head>
 <body>
-    PARABENS VC PASSOU
+    <ul>
+        <li>
+            <a href="formProduto.php">Cadastrar novo produto</a>
+        </li>
+        <li>
+            <a href="listarProdutos.php">Lista de produtos cadastrados</a>
+        </li>
+<br><br>
+        <li>
+            <a href="formCliente.php">Cadastrar novo cliente</a>
+        </li>
+        <li>
+            <a href="listarCliente.php">Lista de clientes cadastrados</a>
+        </li>
+<br><br>
+        <li>
+            <a href="formVenda.php">Cadastrar nova venda</a>
+        </li>
+        <li>
+            <a href="listarVenda.php">Lista de vendas cadastrados</a>
+        </li>
+<br><br>
+        <li>
+            <a href="formIngrediente.php">Cadastrar nova ingrediente</a>
+        </li>
+        <li>
+            <a href="listarIngrediente.php">Lista de ingredientes cadastrados</a>
+        </li>
+<br><br>
+        <li>
+            <a href="deslogar.php">Sair</a>
+        </li>
+    </ul>
 </body>
 </html>
