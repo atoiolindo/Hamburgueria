e7276219881809e6aeb650f09439b06b556eea75
isasmin <liroyasmin@gmail.com>
2025-08-27 08:18:39 -0300

    deixando bonitinho (n vou mais mexer no css)

--- codigo/public/index.php
+++ codigo/public/index.php
@@ -7,7 +7,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="./css/index.css">
     <link href="https://fonts.googleapis.com/css2?family=Bevan:ital,wght@0,400;1,400&display=swap" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
  
     <script>
         function toggleMenu() {
@@ -15,8 +18,9 @@
             menu.classList.toggle('open');
         }
     </script>
+
 </head>
-<body>
+<body class= "p-3 m-0 border-0 bd-example m-0 border-0">
     <button class="menu-btn" onclick="toggleMenu()" title="Menu">
         <i class="fa-solid fa-bars"></i>
     </button>
@@ -46,7 +50,7 @@
         </div>
     </form>
 
-    <h1 style="font-family: 'Bevan', serif;">Hamburgueria Pão & Magia: Sua gordura, nossa alegria!</h1>
+    <h1 style="font-family: 'Bevan', serif;">Hamburgueria Pão & Magia: Sua larica, nossa alegria!</h1>
     <h3>Hambúrgueres artesanais com ingredientes caseiros deliciosos.</h3>
     <a href="cardapio.php" class="btn-cardapio">Peça agora!</a>
 
@@ -60,6 +64,100 @@
     <a href="cardapio.php" class="bolinha">Refrigerantes</a>
 </div>
 
-</body>
 
+
+<h2 style="font-family: 'Bevan', serif; color: #4B1203;">Nossos itens mais pedidos </h2><br><br>
+
+
+<div class="card" style="width: 18rem;">
+    <svg aria-label="Placeholder: Image cap" class="bd-placeholder-img card-img-top" height="180" preserveAspectRatio="xMidYMid slice" role="img" width="100%" xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+    <div class="card-body">
+    <h5 style="font-family: 'Bevan', serif; color: #4B1203;">Feitiço Clássico</h5>
+    <p class="card-text">R$33,00</p>
+    <a href="" class="btn-cardapio">Ver Produto</a>
+    </div>
+</div>
+
+
+<div class="card" style="width: 18rem;">
+    <svg aria-label="Placeholder: Image cap" class="bd-placeholder-img card-img-top" height="180" preserveAspectRatio="xMidYMid slice" role="img" width="100%" xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+    <div class="card-body">
+    <h5 style="font-family: 'Bevan', serif; color: #4B1203;">Encantamento Dourado</h5>
+    <p class="card-text">R$40,00</p>
+    <a href="" class="btn-cardapio">Ver Produto</a>
+    </div>
+</div>
+
+
+<div class="card" style="width: 18rem;">
+    <svg aria-label="Placeholder: Image cap" class="bd-placeholder-img card-img-top" height="180" preserveAspectRatio="xMidYMid slice" role="img" width="100%" xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+    <div class="card-body">
+    <h5 style="font-family: 'Bevan', serif; color: #4B1203;">Trio Arcano</h5>
+    <p class="card-text">R$35,00</p>
+    <a href="" class="btn-cardapio">Ver Produto</a>
+    </div>
+</div>
+
+
+<div class="card" style="width: 18rem;">
+    <svg aria-label="Placeholder: Image cap" class="bd-placeholder-img card-img-top" height="180" preserveAspectRatio="xMidYMid slice" role="img" width="100%" xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+    <div class="card-body">
+    <h5 style="font-family: 'Bevan', serif; color: #4B1203;">Gorgonzola Místico</h5>
+    <p class="card-text">R$42,00</p>
+    <a href="" class="btn-cardapio">Ver Produto</a>
+    </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+    <svg aria-label="Placeholder: Image cap" class="bd-placeholder-img card-img-top" height="180" preserveAspectRatio="xMidYMid slice" role="img" width="100%" xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+    <div class="card-body">
+    <h5 style="font-family: 'Bevan', serif; color: #4B1203;">Batata Frita Média Simples</h5>
+    <p class="card-text">R$13,00</p>
+    <a href="" class="btn-cardapio">Ver Produto</a>
+    </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+    <svg aria-label="Placeholder: Image cap" class="bd-placeholder-img card-img-top" height="180" preserveAspectRatio="xMidYMid slice" role="img" width="100%" xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+    <div class="card-body">
+    <h5 style="font-family: 'Bevan', serif; color: #4B1203;"> Coca-Cola Lata</h5>
+    <p class="card-text">R$5,00</p>
+    <a href="" class="btn-cardapio">Ver Produto</a>
+    </div>
+</div>
+
+<br><br>
+
+<a href="cardapio.php" class="btn-botao">Ver todos os produtos</a> <br><br><br><br>
+
+
+<h2 style="font-family: 'Bevan', serif; color: #4B1203;">Sobre nós</h2><br>
+<p class="sobre-nos"> Somos apaixonados por hambúrgueres e acreditamos que cada mordida deve ser uma experiência inesquecível. Surgimos 
+da vontade de oferecer mais do que refeições rápidas — queremos proporcionar momentos, sabores marcantes e aquele gostinho de quero mais.</p>
+
+<p class="sobre-nos">Na nossa hamburgueria, unimos ingredientes frescos, carnes selecionadas e receitas autorais, sempre preparados com 
+muito carinho e atenção aos detalhes. Nosso ambiente foi pensado para que você se sinta em casa, seja para um almoço descontraído, 
+um encontro entre amigos ou um jantar especial.</p>
+
+<p class="sobre-nos">Aqui, valorizamos a simplicidade e a qualidade. Do pão artesanal às combinações 
+criativas de molhos e acompanhamentos, cada item do nosso cardápio é cuidadosamente elaborado para surpreender.</p>
+<a href="" class="btn-botao">Saiba mais</a> <br><br><br><br>
+
+
+
+</body>
+ <footer>
+ Horários de Funcionamento: <br>
+    Segunda a Sábado: 17h - 22h <br>
+    Domingo: Fechado<br><br>
+ 
+ Informações<br>
+    Sobre Nós<br>
+    Política de Privacidade<br>
+    Endereço<br>
+    <a href="https://access.workspace.google.com/ServiceNotAllowed?application=960906454010&source=scrip&continue=https://www.google.com/maps/place/Rua%2BLuiz%2BFagund
+    es,%2B51%2B-%2BPicadas%2Bdo%2BSul,%2BS%25C3%25A3o%2BJos%25C3%25A9%2B-%2BSC,%2B88104-300/data%3D!4m2!3m1!1s0x9527360529045821:0x780b6dd572a37?sa%3DX%26ved%3D1t:242%
+    26ictx%3D111&marker=c79b2205-0fca-4ee3-890a-f2dc68f5874a&pli=1">Rua Luiz Fagundes, 51 - Praia Comprida, São José - SC, 88103-500</a> <br>
+
+ </footer>
 </html>
