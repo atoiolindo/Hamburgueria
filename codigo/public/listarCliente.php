68522378e301e775cedced222b8e642e09092c04
gab99999 <163896436+gab99999@users.noreply.github.com>
2025-08-28 13:41:11 -0300

    começo do começo do listarclient5e

---
+++ codigo/public/listarCliente.php
@@ -0,0 +1,71 @@
+<?php
+    require_once "verificaLogado.php";
+
+    if ($_SESSION['tipo'] != 'g') {
+        header("Location: home.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Clientes</title>
+    <style>
+        img {
+            width: 50px;
+            height: 50px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Lista de clientes</h1>
+
+    <?php
+    require_once "conexao.php";
+    require_once "funcoes.php";
+
+    $lista_clientes = listarClientes($conexao);
+    
+    //verificar se encontrou clientes antes de imprimir.
+    if (count($lista_clientes) == 0) {
+        echo "Não existem clientes cadastrados.";
+    } else {
+    ?>
+        <table border="1">
+            <tr>
+                <td>Id</td>
+                <td>Foto</td>
+                <td>Nome</td>
+                <td>CPF</td>
+                <td>Endereço</td>
+                <td colspan="2">Ação</td>
+            </tr>
+
+        <?php
+        foreach ($lista_clientes as $cliente) {
+            $idcliente = $cliente['idcliente'];
+            $nome = $cliente['nome'];
+            $cpf = $cliente['cpf'];
+            $endereco = $cliente['endereco'];
+            $telefone = $cliente['foto'];
+
+            echo "<tr>";
+            echo "<td>$idcliente</td>";
+            echo "<td><img src='fotos/$foto'></td>";
+            echo "<td>$nome</td>";
+            echo "<td>$email</td>";
+            echo "<td>$endereco</td>";
+            echo "<td>$telefone</td>";
+            echo "<td><a href='formCliente.php?id=$idcliente'>Editar</a></td>";
+            echo "<td><a href='deletarCliente.php?id=$idcliente'>Excluir</a></td>";
+            echo "</tr>";
+        }
+    }
+        ?>
+        </table>
+</body>
+
+</html>
