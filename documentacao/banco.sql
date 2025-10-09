94113759db7c7f0e299f09011dfd7abbcd937354
atoiolindo <antoniomiguelandrade11@gmail.com>
2025-10-09 14:08:56 -0300

    Revert "insert usuario no banco tmb (tinha esqucido)"
    This reverts commit 82b899c786c46d2d6e607a880ef8631dcc068303.

--- documentacao/banco.sql
+++ documentacao/banco.sql
@@ -395,11 +395,11 @@ INSERT INTO cliente (nome, telefone, endereco, email) VALUES ('Luana Coutto', '6
 INSERT INTO cliente (nome, telefone, endereco, email) VALUES ('Alícia Oliveira', '62 91034 2149', 'Estação de Ramos, 84, Bernadete, 48402260 da Mota de Minas / SP', 'aliciaoliveira1@gmail.com');
 -- INSERTs para usuario
 
-INSERT INTO usuario ( email, senha, nome, tipo) VALUES ( 'jess_rainhe@yahoo.com.br', '$2y$10$MwI4cpcmi6gJhSjxWTzJUejR7MK63EA3SQiRhp//yqzmSIJzIvsYm', 'Jessica Arantes', 'a');
-INSERT INTO usuario ( email, senha, nome, tipo) VALUES ( 'antonio.andrade@gmail.com', '$2y$10$yHqcxeEU22fW1UNYXj.EgejsFuScLbKS/aELrcs4VmWkghlJdS12a', 'Antônio Andrade', 'b');
-INSERT INTO usuario ( email, senha, nome, tipo) VALUES ( 'gabriel.rodrigues@gmail.com', '$2y$10$n5PDU7AAWJYj4KKf0PFK6e6rEQ9ag.yNLxY2Kk9hd8IzbO0cHmaHq', 'Gabriel Rodrigues', 'b');
-INSERT INTO usuario ( email, senha, nome, tipo) VALUES ( 'yasmin.pereira@gmail.com', '$2y$10$JMZ1VlAtGhcPg5wq0cjl4u6lRoP2Gf/ohSKzMXbw4vvvLRn7LGtNW', 'Yasmin Pereira', 'b');
-INSERT INTO usuario ( email, senha, nome, tipo) VALUES ( 'im.just.cleber@gmail.com', '$2y$10$UWktZ11KeApu8Zu.3PddBe7KhUejcR8B/KZYP/LkOn/KpgBc0zILy', 'Cleber', 'c');
+INSERT INTO usuario ( email, senha, nome, tipo, token, status) VALUES ( 'jess_rainhe@yahoo.com.br', '$2y$10$MwI4cpcmi6gJhSjxWTzJUejR7MK63EA3SQiRhp//yqzmSIJzIvsYm', 'Jessica Arantes', 'a', '1', 'não');
+INSERT INTO usuario ( email, senha, nome, tipo, token, status) VALUES ( 'antonio.andrade@gmail.com', '$2y$10$yHqcxeEU22fW1UNYXj.EgejsFuScLbKS/aELrcs4VmWkghlJdS12a', 'Antônio Andrade', 'b', '2', 'não');
+INSERT INTO usuario ( email, senha, nome, tipo, token, status) VALUES ( 'gabriel.rodrigues@gmail.com', '$2y$10$n5PDU7AAWJYj4KKf0PFK6e6rEQ9ag.yNLxY2Kk9hd8IzbO0cHmaHq', 'Gabriel Rodrigues', 'b', '3', 'não');
+INSERT INTO usuario ( email, senha, nome, tipo, token, status) VALUES ( 'yasmin.pereira@gmail.com', '$2y$10$JMZ1VlAtGhcPg5wq0cjl4u6lRoP2Gf/ohSKzMXbw4vvvLRn7LGtNW', 'Yasmin Pereira', 'b', '4', 'não');
+INSERT INTO usuario ( email, senha, nome, tipo, token, status) VALUES ( 'im.just.cleber@gmail.com', '$2y$10$UWktZ11KeApu8Zu.3PddBe7KhUejcR8B/KZYP/LkOn/KpgBc0zILy', 'Cleber', 'c', '5', 'não');
 -- INSERTs para venda
 
 INSERT INTO venda ( valor_final, observacao, data, idcliente, status)
