a81da02ced309bfdf748cc655609070cf677a671
yasmin <liroyasmin@gmail.com>
2025-11-03 21:17:26 -0300

    arrumando css de algumas pag

--- codigo/public/cartao.php
+++ codigo/public/cartao.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title>Meio de Pagamento </title>
+<link rel="stylesheet" href="./css/cartao.css">
+
 </head>
 <body>
-    escolha um meio de pagamento 
-
-   <a href="credito.php">Crédito</a> 
-   <a href="debito.php">Débito</a> 
+  <header>
+  </header>
+  <main>
+    <h2>Escolha um meio de pagamento</h2>
+    <div class="payment-options">
+      <div class="payment-option">
+        <div class="icon-container">
+          <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 7H4a1 1 0 0 0-1 1v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-8a1 1 0 0 0-1-1zM9 17H7v-2h2v2zm9-3h-8v-2h8v2z" /></svg>
+        </div>
+        Crédito
+      </div>
+      <div class="payment-option">
+        <div class="icon-container">
+          <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 7H4a1 1 0 0 0-1 1v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-8a1 1 0 0 0-1-1zM9 17H7v-2h2v2zm9-3h-8v-2h8v2z" /></svg>
+        </div>
+        Débito
+      </div> 
+    </div>
+  </main>
 </body>
 </html>
--- codigo/public/pagamento.php
+++ codigo/public/pagamento.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title>Formas de pagamento</title>
+<link rel="stylesheet" href="./css/pagamento.css">
 </head>
 <body>
-    formas de pagamento
-    adicione um cartão
-
-
-     <a href="cartao.php">adicionar novo cartao</a>
+  <h1>Formas de pagamento</h1>
+  <div class="container">
+    <div class="text-content">
+      <h2>Adicione um cartão</h2>
+      <p>É prático, seguro e você não perde nenhum minuto a mais quando seu pedido chegar.</p>
+      <button class="btn" onclick="window.location.href='cartao.php'">
+       Adicionar novo cartão
+      </button>   
+    </div>
+    <div class="image">
+      <svg class="hand" viewBox="0 0 180 140" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M40 70c0-38 18-62 40-55 18 5 38 45 60 40 15-5 30-25 45-5 20 25 0 55-20 72-18 14-37 10-45 5-10-8-15-20-20-30-8-17-15-20-13-30Z" fill="#F2F2F2"/>
+        <path d="M40 70c10-25 20-50 40-53 15-3 25 10 30 15 15 15 40 10 45 10 0 15 0 20-10 25-15 8-20 5-28 3-15-5-15-20-20-30-8-17-15-20-13-30Z" fill="#A16A99"/>
+        <rect x="68" y="75" width="60" height="30" rx="8" ry="8" fill="#E9202D"/>
+        <rect x="68" y="75" width="60" height="8" rx="2" ry="2" fill="#7D1722"/>
+      </svg>
+    </div>
+  </div>
 </body>
 </html>
